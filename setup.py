5c2d0686f7e10c11e3da0a4e1bc35a345e6ccb2b
Jacob Schreiber <jmschreiber91@gmail.com>
2016-04-20 23:51:01 -0700

    setup added

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy as np
+
+extensions = [
+            Extension( "blueberry.*", 
+                       [ "blueberry/blueberry.pyx" ], 
+                       include_dirs=[np.get_include()] )
+    ]
+
+extensions = cythonize( extensions )
+
+setup(
+    name='blueberry',
+    version='0.4.0',
+    author='Jacob Schreiber',
+    author_email='jmschreiber91@gmail.com',
+    packages=['blueberry'],
+    url='http://pypi.python.org/pypi/blueberry/',
+    license='LICENSE.txt',
+    description='Blueberry is a package for the analysis of the 3D structure of the genome.',
+    ext_modules=extensions,
+    install_requires=[
+        "cython >= 0.22.1",
+        "numpy >= 1.8.0",
+        "joblib >= 0.9.0b4",
+        "mxnet >= 0.5a3"
+    ],
+)
