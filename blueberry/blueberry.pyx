39cc1ec159a5254c83054cba7260edb90b82e4e0
Jacob Schreiber <jmschreiber91@gmail.com>
2016-11-22 14:41:55 -0800

    a

--- blueberry/blueberry.pyx
+++ blueberry/blueberry.pyx
@@ -93,7 +93,7 @@ cpdef count_band_regions( numpy.ndarray regions_ndarray ):
 def predict(name, iteration, ctx, n_jobs, bint use_seq=True, bint use_dnase=True, 
 	bint use_dist=True, int min_dist=25000, int max_dist=10000000):
 	cdef int batch_size = 10240, window = 1000, width = 500
-	cdef int k, tot = 0, i, j, l, mid1, mid2, 
+	cdef int k = 0, tot = 0, i, j, l, mid1, mid2, 
 	cdef numpy.ndarray sequence = numpy.load('/data/scratch/ssd/jmschr/contact/chr21.ohe.npy')
 	cdef numpy.ndarray dnase = numpy.load('/data/scratch/ssd/jmschr/contact/chr21.GM12878.ohe_dnase.npy')
 	cdef numpy.ndarray mid2_regions = numpy.load('/data/scratch/ssd/jmschr/contact/chr21.GM12878.regions.1000.npy').astype('int')
