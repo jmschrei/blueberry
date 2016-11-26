dda2bec455598b83247fcefa8014fe4b8398e60e
Jacob Schreiber <jmschreiber91@gmail.com>
2016-11-26 01:14:07 -0800

    a

--- blueberry/models.pyx
+++ blueberry/models.pyx
@@ -417,34 +417,34 @@ def Rambutan(**kwargs):
 	x1seq = Variable(name="x1seq")
 	x1dnase = Variable(name="x1dnase")
 
-	x1seq = Convolution(x1seq, 48, (7, 4), pad=(3, 0))
+	x1seq = Convolution(x1seq, 64, (7, 4), pad=(3, 0))
 	x1seq = Pooling(x1seq, kernel=(3, 1), stride=(3, 1), pool_type='max')
-	x1seq = Convolution(x1seq, 48, (7, 1), pad=(3, 0))
+	x1seq = Convolution(x1seq, 64, (7, 1), pad=(3, 0))
 	x1seq = Pooling(x1seq, kernel=(3, 1), stride=(3, 1), pool_type='max')
 
 	x1dnase = Pooling(x1dnase, kernel=(9, 1), stride=(9, 1), pool_type='max')
 	x1dnase = Convolution(x1dnase, 4, (1, 8))
 
 	x1 = Concat(x1seq, x1dnase)
-	x1 = Convolution(x1, 64, (3, 1))
-	x1 = Convolution(x1, 64, (3, 1))
+	x1 = Convolution(x1, 96, (3, 1))
+	x1 = Convolution(x1, 96, (3, 1))
 	x1 = Flatten(Pooling(x1, kernel=(107, 1), stride=(107, 1), pool_type='max'))
 	x1 = Dense(x1, 256)
 
 	x2seq = Variable(name="x2seq")
 	x2dnase = Variable(name="x2dnase")
 
-	x2seq = Convolution(x2seq, 48, (7, 4), pad=(3, 0))
+	x2seq = Convolution(x2seq, 64, (7, 4), pad=(3, 0))
 	x2seq = Pooling(x2seq, kernel=(3, 1), stride=(3, 1), pool_type='max')
-	x2seq = Convolution(x2seq, 48, (7, 1), pad=(3, 0))
+	x2seq = Convolution(x2seq, 64, (7, 1), pad=(3, 0))
 	x2seq = Pooling(x2seq, kernel=(3, 1), stride=(3, 1), pool_type='max')
 
 	x2dnase = Pooling(x2dnase, kernel=(9, 1), stride=(9, 1), pool_type='max')
 	x2dnase = Convolution(x2dnase, 4, (1, 8))
 
 	x2 = Concat(x2seq, x2dnase)
-	x2 = Convolution(x2, 64, (3, 1))
-	x2 = Convolution(x2, 64, (3, 1))
+	x2 = Convolution(x2, 96, (3, 1))
+	x2 = Convolution(x2, 96, (3, 1))
 	x2 = Flatten(Pooling(x2, kernel=(107, 1), stride=(107, 1), pool_type='max'))
 	x2 = Dense(x2, 256)
 
