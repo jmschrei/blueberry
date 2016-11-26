898d029f83b0fc4501a01f37fe6290f8e4b04ec8
Jacob Schreiber <jmschreiber91@gmail.com>
2016-11-25 23:33:27 -0800

    a

--- blueberry/models.pyx
+++ blueberry/models.pyx
@@ -426,8 +426,9 @@ def Rambutan(**kwargs):
 	x1dnase = Convolution(x1dnase, 4, (1, 8))
 
 	x1 = Concat(x1seq, x1dnase)
-	x1 = Convolution(x1, 48, (3, 1))
-	x1 = Flatten(Pooling(x1, kernel=(109, 1), stride=(109, 1), pool_type='max'))
+	x1 = Convolution(x1, 64, (3, 1))
+	x1 = Convolution(x1, 64, (3, 1))
+	x1 = Flatten(Pooling(x1, kernel=(107, 1), stride=(107, 1), pool_type='max'))
 	x1 = Dense(x1, 256)
 
 	x2seq = Variable(name="x2seq")
@@ -442,8 +443,9 @@ def Rambutan(**kwargs):
 	x2dnase = Convolution(x2dnase, 4, (1, 8))
 
 	x2 = Concat(x2seq, x2dnase)
-	x2 = Convolution(x2, 48, (3, 1))
-	x2 = Flatten(Pooling(x2, kernel=(109, 1), stride=(109, 1), pool_type='max'))
+	x2 = Convolution(x2, 64, (3, 1))
+	x2 = Convolution(x2, 64, (3, 1))
+	x2 = Flatten(Pooling(x2, kernel=(107, 1), stride=(107, 1), pool_type='max'))
 	x2 = Dense(x2, 256)
 
 	xd = Variable(name="distance")
